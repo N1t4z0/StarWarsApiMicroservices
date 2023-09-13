8b073fe35f35e4b55ad7ab6cd566b9e70ee789c2
N1t4z0 <114629649+N1t4z0@users.noreply.github.com>
2023-09-13 17:53:08 -0300

    microservice funcionando

--- gateaway/index.js
+++ gateaway/index.js
@@ -4,15 +4,15 @@ const {createProxyMiddleware} = require("http-proxy-middleware");
 const app = express();
 
 app.use("/characters", createProxyMiddleware({
-	target:"http://localhost:8001",
+	target:"http://characters:8001",
 	changeOrigin:true
 }));
 app.use("/films", createProxyMiddleware({
-	target:"http://localhost:8002",
+	target:"http://films:8002",
 	changeOrigin:true
 }));
 app.use("/planets", createProxyMiddleware({
-	target:"http://localhost:8003",
+	target:"http://planets:8003",
 	changeOrigin:true
 }));
 
