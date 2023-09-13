54133512228c29a824c134a1aa34a79488b6983c
N1t4z0 <114629649+N1t4z0@users.noreply.github.com>
2023-09-13 15:12:54 -0300

    microserviceamos todo pai

---
+++ gateaway/index.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const {createProxyMiddleware} = require("http-proxy-middleware");
+
+const app = express();
+
+app.use("/characters", createProxyMiddleware({
+	target:"http://localhost:8001",
+	changeOrigin:true
+}));
+app.use("/films", createProxyMiddleware({
+	target:"http://localhost:8002",
+	changeOrigin:true
+}));
+app.use("/planets", createProxyMiddleware({
+	target:"http://localhost:8003",
+	changeOrigin:true
+}));
+
+app.listen(8000)
